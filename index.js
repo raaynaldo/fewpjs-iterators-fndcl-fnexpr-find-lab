0564f272364c23419c171d79d2a8f6fc16b4abaa
raaynaldo <raaynaldo@hotmail.com>
2020-11-16 18:05:06 -0500

    commit tree a6cfff531fce069ed9dcf38a2d7f1f5baa56de57

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(records){
+  const win = records.find( function(record){
+    return record["result"] == "W"
+  });
+
+  return win && win["year"]
+}
